3e7d8b61442e29bb918a64ab075732c4588f8412
Al sameu <alsameu150@gmail.com>
2021-06-13 16:00:37 +0600

    fourth commit

---
+++ lab 3/problem 3.php
@@ -0,0 +1,14 @@
+<?php
+$length=10;
+$width=10;
+
+$perimeter= 2*($length+$width);
+echo "Perimeter: $perimeter<br>";
+
+$area= ($length*$width);
+echo "Area: $area<br>";
+
+if($length===$width){
+echo "the shape is a square";
+}
+?>
---
+++ lab 3/problem 4.php
@@ -0,0 +1,11 @@
+<?php
+$name="mithu";
+
+if($name=="raju" or $name=="mina" or $name=="mithu"){
+echo "Hello!";
+}
+else
+{
+echo "You are not recognized";
+}
+?>
