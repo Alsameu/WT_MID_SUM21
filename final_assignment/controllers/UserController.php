4efe82fd5d27cb25866908ec44cc0db5bbf5e287
Al sameu <alsameu150@gmail.com>
2021-07-13 20:10:13 +0600

    final assignment done

--- final_assignment/controllers/UserController.php
+++ final_assignment/controllers/UserController.php
@@ -1,4 +1,5 @@
 <?php
+include 'models/db_config.php';
 	$name="";
 	$err_name="";
 	$uname="";
--- final_assignment/signup.php
+++ final_assignment/signup.php
@@ -1,33 +1,34 @@
 <?php include 'main_header.php';?>
 <?php
-include 'controllers/temp.php';
+include 'controllers/UserController.php';
 ?>
-<!--sign up starts -->
-<div class="center-login">
- <h1 class="text text-center">Sign Up</h1>
- <form action="" method="post" class="form-horizontal form-material"> 
+  <!--sign up starts -->
+  <div class="center-login">
+  <h1 class="text text-center">Sign Up</h1>
+  <h5 class="text-danger"><?php echo $err_db?></h5>
+  <form action="" method="post" class="form-horizontal form-material">
   <div class="form-group">
   <h4 class="text">Name</h4>
   <input type="text" name="name" value="<?php echo $name;?>" class="form-control">
   <span class="text-danger"><?php echo $err_name;?></span>
-</div>  
- <div class="form-group">
+  </div>  
+  <div class="form-group">
   <h4 class="text">Username</h4>
- <input type="text" name="uname" value="<?php echo $uname;?>" class="form-control">
+  <input type="text" name="uname" value="<?php echo $uname;?>" class="form-control">
   <span class="text-danger"><?php echo $err_uname;?></span>
-</div>
- <div class="form-group">
+  </div>
+  <div class="form-group">
   <h4 class="text">Email</h4>
- <input type="text" name="email" value="<?php echo $email;?>" class="form-control">
+  <input type="text" name="email" value="<?php echo $email;?>" class="form-control">
   <span class="text-danger"><?php echo $err_email;?></span>
-</div>
- <div class="form-group">
+  </div>
+  <div class="form-group">
   <h4 class="text">Password</h4>
- <input type="password" name="password" value="<?php echo $password;?>" class="form-control">
+  <input type="password" name="password" value="<?php echo $password;?>" class="form-control">
   <span class="text-danger"><?php echo $err_password;?></span>
-</div>
-<div class="form-group text-center">
-<input type ="submit" name="sign_up" class="btn btn-sucess" value="Sign Up" class="form-control">
+  </div>
+  <div class="form-group text-center">
+  <input type ="submit" name="sign_up" class="btn btn-sucess" value="Sign Up" class="form-control">
   </div>
   </form>
   </div>
